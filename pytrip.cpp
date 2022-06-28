3c988e40ed7c2d843ba8ad2b113307501c8a6717
Vaishnavi Saindane <71937373+vaishnavisaindane@users.noreply.github.com>
2022-06-29 00:25:10 +0530

    Create pytrip.cpp
    function to find pythagorian triplet

---
+++ pytrip.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool check(int x,int y,int z ){
+    int a= max(x,max(y,z));
+    int b,c;
+    if(a==x)
+    {
+        b=y;
+        c=z;
+    }
+    if(a==y){
+        b=x;
+        c=z;
+    }
+    else{
+        b=x;
+        c=y;
+    }
+    if(a*a==b*b+c*c)
+    return true;
+    else
+    return false;
+    
+}
+
+int32_t main()
+{
+   int x,y,z;
+   cin>>x>>y>>z;
+   if(check(x,y,z)){
+       cout<<"pythagorian triplet";
+   }
+   else
+   cout<<"not a pythagorian triplet";
+    return 0;
+}
